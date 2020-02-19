77473930e36e5cead1014d2baf486446200e680d
vcunningham <wolf1023@gmail.com>
2020-02-19 13:40:59 -0600

    Activity 5 Completed
    Fully functioning

--- js/main.js
+++ js/main.js
@@ -1,8 +1,43 @@
-var map = L.map('mapid').setView([20,0], 2);
+// create the map with the desired view and zoom
+var map = L.map('mapid').setView([38,16], 4);
 
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox/streets-v11',
-    accessToken: 'pk.eyJ1IjoidmN1bm5pbmdoYW0iLCJhIjoiY2s2anZ0bDV5MDBmOTNxcHc4YTlzcDZ6cSJ9.AtmCmgJx0lPwaMrh172guQ'
+// load the tileset I found
+L.tileLayer('https://{s}.tile.thunderforest.com/neighbourhood/{z}/{x}/{y}.png?apikey={apikey}', {
+	attribution: '&copy; <a href="http://www.thunderforest.com/">Thunderforest</a>, &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+	apikey: '5905384e78ab4358b59022a245531c74',
+	maxZoom: 22
 }).addTo(map);
+
+// I copied in the basic feature click to show properties function
+function onEachFeature(feature, layer) {
+    //no property named popupContent; instead, create html string with all properties
+    var popupContent = "";
+    if (feature.properties) {
+        //loop to add feature property names and values to html string
+        for (var property in feature.properties){
+            popupContent += "<p>" + property + ": " + feature.properties[property] + "</p>";
+        }
+        layer.bindPopup(popupContent);
+    };
+};
+
+// load the geojson I made from my data into the map
+$.getJSON("data/map_data.geojson", function(response){
+            //create marker options
+            var geojsonMarkerOptions = {
+                radius: 8,
+                fillColor: "#ff7800",
+                color: "#000",
+                weight: 1,
+                opacity: 1,
+                fillOpacity: 0.8
+            };
+
+            //create a Leaflet GeoJSON layer and add it to the map
+            L.geoJson(response, {
+				onEachFeature: onEachFeature,
+                pointToLayer: function (feature, latlng){
+                    return L.circleMarker(latlng, geojsonMarkerOptions);
+                }
+            }).addTo(map);
+        });
